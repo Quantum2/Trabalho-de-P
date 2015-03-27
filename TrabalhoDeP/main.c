219833a8fee009d714415617bb23e74854d41d87
Quantum2 <rafaelfrancisco6@gmail.com>
2015-03-27 21:22:07 +0000

    Verificaçao de dados

--- TrabalhoDeP/main.c
+++ TrabalhoDeP/main.c
@@ -1,12 +1,16 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 #include "vector.h"
 #include "ins_dados.h"
 
+#define TEMPO_DE_PAUSA 2
+
 int main(int argc, char *argv[]){
 	if (welcome_conf() != 0){
-		printf("");
+		printf("Os dados inseridos sao invalidos, tente de novo\n");
+		Sleep(TEMPO_DE_PAUSA * 1000);
 	}
 
 	return 0;

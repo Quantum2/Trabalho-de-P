6d83c650122a87a3bae572b1340125bd83dfeaff
Quantum2 <rafaelfrancisco6@gmail.com>
2015-03-27 20:47:50 +0000

    First Commit

---
+++ TrabalhoDeP/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vector.h"
+
+int main(int argc, char *argv[]){
+	int n_pessoas;
+	vector v1;
+
+	printf("Bem vindo, quantas pessoas existem na simulacao ?\n");
+	scanf_s("%d", &n_pessoas);
+
+	return 0;
+}
